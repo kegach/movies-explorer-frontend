fa6df4f2bf6be28654294e9b891ad6409630aa3f
Mike Pugach <kegach@yandex.ru>
2021-03-25 18:47:21 +0300

    Diplom movies3

--- src/utils/MoviesApi.js
+++ src/utils/MoviesApi.js
@@ -1,36 +1,17 @@
-const BASE_URL = "https://api.kegach-diplom.students.nomoredomains.rocks";
-const API_HEADERS = {
-  "Content-Type": "application/json",
-  Accept: "application/json",
+const BASE_URL = 'https://api.nomoreparties.co';
+const headers = {
+  'Content-Type': 'application/json'
 };
-const API_CREDENTIALS = "include";
 
-
-class MoviesApi {
-  constructor(options) {
-    this._baseUrl = options.baseUrl;
-    this._headers = options.headers;
-    this._credentials = options.credentials;
-  }
-
-  async _getRes(res) {
+export function getBeatMovies() {
+  return fetch(`${BASE_URL}/beatfilm-movies`, {
+    headers,
+    credentials: 'same-origin',
+  })
+  .then(res => {
     if (res.ok) {
       return res.json();
     }
-    const errorMessage = await res.json();
-    return Promise.reject(new Error(`Ошибка: ${errorMessage.message}`));
-  }
-
-  async getBeatMovies() {
-    const res = await fetch(this._baseUrl);
-    return this._getRes(res);
-  }
-}
-
-const movieApi = new MoviesApi({
-  baseUrl: BASE_URL,
-  headers: API_HEADERS,
-  credentials: API_CREDENTIALS,
-});
-
-export default movieApi;
+    return Promise.reject(`Ошибка: ${res.status}`);
+  }); 
+};
